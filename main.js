34269b8aa1c7fa1e4b28e955faba0e57afaf1c67
Sinjihn Smith <sinjihnsmith@gmail.com>
2015-11-05 19:57:30 -0500

    added collections to main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+User = new Mongo.Collection("user"); 
+Course = new Mongo.Collection("course");
+StudySpace = new Mongo.Collection("studyspace");
+StudySession = new Mongo.Collection("studysession");
--- routes.js
+++ routes.js
@@ -1,3 +1,60 @@
 Router.route('/', function () {
   this.render('login');
 });
+
+Router.configure({
+	fastRender: true,
+	notFoundTemplate: 'notFound',
+	onBeforeAction: function() {
+		Tooltips.hide(); // hide current tooltips
+		this.next();
+	},
+	// subscriptions: function() {
+	// 	if (Meteor.userId()) {
+	// 		return [
+	// 			// Meteor.subscribe('cards'),
+	// 			//Meteor.subscribe('statuses')
+	// 		]
+	// 	}
+	// }
+});
+
+
+Router.onBeforeAction(function () {
+	//
+	if (!Meteor.userId()) {
+		//ceck to make sure logged in
+		if (Router.current().route.path() === '/login-legal'){ // allow logged out users to see the legal page
+			this.redirect('/login-legal');
+			this.next();
+		}
+		else {
+			this.redirect('/login');
+		}
+
+	} 
+	else if (Meteor.user() && Meteor.user().username && Meteor.user().username === 'default') {
+		this.redirect('/settings');
+		this.next();
+	}
+	else {
+		this.next();
+	}
+}, 
+{
+	except: ['login']
+});
+
+Router.route('/login', {
+	name: 'login',
+	action: function() {
+		if (Meteor.userId()) {
+			var currentDate = moment(new Date()).hour(0).minute(0).second(0).milliseconds(0).toDate(); // set date to current day
+			Session.set('date', currentDate);
+			Session.set('status', "");
+			this.redirect('/');
+		}
+		this.layout('splashLayout');
+		this.render('login')
+	}
+});
